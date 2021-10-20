1a39e835300bb47b41d38505134b084c54514471
Aryan Goyal <Aryanlovesmath@gmail.com>
2021-10-20 13:55:17 -0400

    Fixed

--- Completed/trig1/trig.js
+++ Completed/trig1/trig.js
@@ -8,5 +8,12 @@ function AddToValues(){
 }
 
 function CreateTable(){
+    let output = document.getElementById('table');
+    output.innerHTML = '<tr><th>Angle</th><th>Sin &theta;</th><th>Cos &theta;</th><th>Tan &theta;</th></tr>';
     
+    for(let i = 0; i < (values.length + 1); i++)
+    {
+        let radian = values[i] * (Math.PI/180);
+        output.innerHTML = '<tr><td>' + values[i].tofixed(2) + '</td><td>' + Math.sin(radian).toFixed(2)+ '</td><td>' + Math.cos(radian).toFixed(2) + '</td><td>' + Math.tan(radian).toFixed(2) + '</td></tr>';
+    } 
 }
